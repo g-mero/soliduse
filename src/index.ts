ff1ea8e4d60075064fd896ff3c99a06462d44310
Gmero <2431775986@qq.com>
2024-06-30 03:19:30 +0800

    forgot build

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import useInterval from './useInterval'
 import useIntervalFn from './useIntervalFn'
 import useTimeoutFn from './useTimeoutFn'
 import useEventListener from './useEventListener'
+import { onClickOutside } from './onClickOutside'
 
-export { createGlobalState, createComponentState, useEventListener, useIntervalFn, useTimeoutFn, useInterval }
+export { createGlobalState, createComponentState, useEventListener, useIntervalFn, useTimeoutFn, useInterval, onClickOutside }
