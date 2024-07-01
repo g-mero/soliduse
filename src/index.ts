62596d8bea34be9f95715853891dad77a82e9368
gmero <gmero@foxmail.com>
2024-07-01 11:33:51 +0800

    foget export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import useInterval from './useInterval'
 import useIntervalFn from './useIntervalFn'
 import useTimeoutFn from './useTimeoutFn'
 import useEventListener from './useEventListener'
+import watch from './watch'
 import { onClickOutside } from './onClickOutside'
 
-export { createGlobalState, createComponentState, useEventListener, useIntervalFn, useTimeoutFn, useInterval, onClickOutside }
+export { createGlobalState, createComponentState, useEventListener, useIntervalFn, useTimeoutFn, useInterval, onClickOutside, watch }
