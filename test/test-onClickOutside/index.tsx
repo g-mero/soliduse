0d4b2ac780d06db8071fe1b61233671660be759a
gmero <gmero@foxmail.com>
2024-07-01 09:15:45 +0800

    fix click outside

--- test/test-onClickOutside/index.tsx
+++ test/test-onClickOutside/index.tsx
@@ -5,27 +5,21 @@ export default function TestOnClickOutside() {
   let ref1!: HTMLDivElement
   let ref2!: HTMLDivElement
   const [count, setCount] = createSignal(0)
-  const [count2, setCount2] = createSignal(0)
-  const [count3, setCount3] = createSignal(0)
+  const [count2, _setCount2] = createSignal(0)
+  const [count3, _setCount3] = createSignal(0)
 
   onMount(() => {
     onClickOutside(ref, () => {
       setCount(count() + 1)
+    }, {
+      ignore: [ref1, ref2],
     })
-
-    onClickOutside(ref1, () => {
-      setCount2(count2() + 1)
-    }, { detectIframe: true })
-
-    onClickOutside(ref2, () => {
-      setCount3(count3() + 1)
-    }, { detectIframe: true })
   })
 
   return (
-    <div ref={ref} class="p-4 bg-gray-200">
+    <div class="p-4 bg-gray-200">
       <div>Click outside to increment count</div>
-      <div>
+      <div ref={ref}>
         Count:
         {count()}
       </div>
