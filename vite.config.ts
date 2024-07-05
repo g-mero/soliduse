637e2b877cde1b6b3c31bbf234d8202807fb33fd
Gmero <2431775986@qq.com>
2024-07-05 21:36:51 +0800

    tsup stuff

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ command }) => {
       },
     },
     plugins: [
-
       command === 'serve' && UnoCSS({
         presets: [presetUno()],
       }),
@@ -29,21 +28,5 @@ export default defineConfig(({ command }) => {
       }),
 
     ],
-    // 打包配置
-    build: {
-      lib: {
-        entry: 'src/index.ts', // 设置入口文件
-        name: 'easy-context', // 起个名字，安装、引入用
-        formats: ['es'], // 输出格式
-        fileName: () => `index.js`, // 打包后的文件名
-      },
-
-      sourcemap: false, // 输出.map文件
-      rollupOptions: {
-        // 确保外部化处理那些你不想打包进库的依赖
-        external: ['solid-js', 'solid-js/store', 'solid-js/web'],
-      },
-
-    },
   }
 })
